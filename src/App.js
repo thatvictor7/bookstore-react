60d338ecd553c8fab7f322a82d3b3208c9dc86c3
thatvictor7 <montoya.victor7@gmail.com>
2018-12-16 01:03:27 -0700

    added display navs components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col } from 'react-bootstrap'
+import Navs from './components/navs'
+import Display from './components/display'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navs />
+        <Row>
+          <Col xs={8}>
+            <Display />
+          </Col>
+          <Col>3 of 3</Col>
+        </Row>
       </div>
     );
   }
